8a54637a0922549ad28fbc6b7e6482e776752900
odaniel1 <owendnl@gmail.com>
2019-11-29 17:16:05 +0000

    Document magoutcomes data

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Magistrates court outcomes.
+#'
+#' Outcomes for all defendants appearing at magistrates courts in England and Wales in 2018, published by the Ministry of Justice.
+#' Names have been cleaned for easy use with R, and additional fields for the region and country of the relevant police force area added.
+#'
+#' @source \url{https://www.gov.uk/government/statistics/criminal-justice-system-statistics-quarterly-december-2018}
+"magoutcomes"
